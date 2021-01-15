c5264c114937a94ec1cec85a93e1b6a455297197
Alexander Glyantsev <art.minax@gmail.com>
2021-01-15 21:37:46 +0300

    Task 25. Готовые библиотеки компонентов. Material UI
    1. Установи Material UI в проект.
    
    2. Подключи любой из понравившихся готовых компонентов Material UI в свой проект.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import styles from './Item.module.css'
+import { ListItem, ListItemIcon, ListItemText, Checkbox, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/DeleteOutlined';
+import styles from './Item.module.css';
 import classnames from 'classnames';
 
-const Item = ({ value, isDone }) => (
-    <span className={
-        classnames({
-            [styles.todo__item]: true,
-            [styles.todo__item_done]: isDone
-        })
-    }>
-        {value}
-    </span>
-);
+export default function Item({ id, value, isDone }) {
+    const [checked, setChecked] = React.useState([1]);
 
-export default Item;
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
+    return (
+        <ListItem role={undefined} dense onClick={handleToggle(value)}
+            className={
+                classnames({
+                    [styles.todo__item]: true,
+                    [styles.todo__item_done]: isDone
+                })
+            }>
+            <ListItemIcon>
+                <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                />
+            </ListItemIcon>
+            <ListItemText id={id} primary={value} />
+            <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon color="secondary" />
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
+};
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List'
 import Item from '../Item/Item'
 
-const ItemList = ({ items }) => (
-    <ul>
-        {items.map(item => (
-            <li key={item.id}>
-                <Item value={item.value} isDone={item.isDone} />
-            </li>))}
-    </ul>
-);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.dark,
+    },
+}));
 
-export default ItemList;
+export default function ItemList({ items }) {
+    const classes = useStyles();
+
+    return (
+        <List dense className={classes.root}>
+            {items.map(item => (
+                <Item key={item.id} value={item.value} isDone={item.isDone} />
+            ))};
+        </List>
+    );
+};
